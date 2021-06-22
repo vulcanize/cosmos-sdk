10fbfae600c092cc5a96a4bc23a14093d407eee8
antstalepresh <36045227+antstalepresh@users.noreply.github.com>
2021-06-23 06:26:58 +1000

    min commission rate check on create validator (#11)
    add MustUpdateValidatorCommission

--- x/staking/keeper/msg_server_test.go
+++ x/staking/keeper/msg_server_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/bank/testutil"
 	"github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/stretchr/testify/require"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
@@ -143,4 +144,33 @@ func TestCancelUnbondingDelegation(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCreateValidatorWithLessThanMinCommission(t *testing.T) {
+	PKS := simapp.CreateTestPubKeys(1)
+	valConsPk1 := PKS[0]
+
+	app := simapp.Setup(t, false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	addrs := simapp.AddTestAddrs(app, ctx, 3, sdk.NewInt(1234))
+
+	// set min commission rate to non-zero
+	params := app.StakingKeeper.GetParams(ctx)
+	params.MinCommissionRate = sdk.NewDecWithPrec(1, 2)
+	app.StakingKeeper.SetParams(ctx, params)
+
+	// create validator with 0% commission
+	msg, err := stakingtypes.NewMsgCreateValidator(
+		sdk.ValAddress(addrs[0]),
+		valConsPk1,
+		sdk.NewInt64Coin(sdk.DefaultBondDenom, 100),
+		stakingtypes.Description{},
+		stakingtypes.NewCommissionRates(sdk.NewDec(0), sdk.NewDecWithPrec(5, 1), sdk.NewDec(0)),
+		sdk.OneInt())
+	require.NoError(t, err)
+
+	svr := keeper.NewMsgServerImpl(app.StakingKeeper)
+	_, err = svr.CreateValidator(ctx, msg)
+	require.Error(t, err)
 }
